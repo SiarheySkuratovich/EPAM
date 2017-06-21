3d659b269e1aec3980bfd7804aa54279807f4fb2
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-06-21 17:09:41 +0300

    Update Dev_1.java

--- Dev_1.java
+++ Dev_1.java
@@ -1,15 +1,11 @@
 public class Dev_1 {
-    public static void main(String[] args){
-        int temp;
-        for(int i = 0; i <= 100; i++) {
-            if(i % 3 == 0) {
-                temp = i / 3;
-                System.out.println("3*" + temp);
-            }
-            else {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("3*" + i / 3);
+            } else {
                 System.out.println(i);
             }
-
         }
     }
 }
