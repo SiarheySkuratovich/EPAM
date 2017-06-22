764740ba326f371efe6f3bfb28811b581fac2cd6
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-06-22 14:47:49 +0300

    Update and rename Dev_2.java to Dev_1.java

---
+++ Dev_1.java
@@ -0,0 +1,15 @@
+public class Dev_1 {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      if (i % 3 == 0 && i % 5 != 0) {
+        System.out.println("Tutti");
+      } else if (i % 5 == 0 && i % 3 != 0) {
+        System.out.println("Frutti");
+      } else if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("Tutti-Frutti");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
