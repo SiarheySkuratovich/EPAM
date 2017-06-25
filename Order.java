bde581fc7a4daa99d0ffd36fa4415b137433f8fe
SiarheySkuratovich <stalker15118@mail.ru>
2017-06-25 17:07:50 +0300

    number of numbers

--- Order.java
+++ Order.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Order {
   public static void main(String[] args) {
-  System.out.println("Write down any order of numbers");
-  Scanner in = new Scanner(System.in);
-  String order = in.nextLine();
-  System.out.println("you have written: " + order);
+    System.out.println("Write down any order of numbers");
+    Scanner in = new Scanner(System.in);
+    String order = in.nextLine();
+    System.out.println("you have written: " + order);
+    int Length = order.length();
+    char orderArray[] = order.toCharArray();
+    int q = 0;
+    for (int i = 0; i < Length ; i++) {
+      if (orderArray[i] == ' ') {
+        q++;
+      }
+    }
+    System.out.println(q + 1);
   }
 }
