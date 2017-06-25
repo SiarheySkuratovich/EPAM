4ad5e73de3661a5eb9e79930d9248cf07f381075
SiarheySkuratovich <stalker15118@mail.ru>
2017-06-25 17:39:00 +0300

    my own uncompleted  algorithm

--- Order.java
+++ Order.java
@@ -7,13 +7,22 @@ public class Order {
     String order = in.nextLine();
     System.out.println("you have written: " + order);
     int Length = order.length();
-    char orderArray[] = order.toCharArray();
+    char strArray[] = order.toCharArray();
     int q = 0;
     for (int i = 0; i < Length ; i++) {
-      if (orderArray[i] == ' ') {
+      if (strArray[i] == ' ') {
         q++;
       }
     }
-    System.out.println(q + 1);
+    System.out.println("there are " + (q + 1) + " numbers");
+    int numberArray[] = new int[q];
+    String temp;
+    for (int i = 0; i < Length ; i++) {
+      if (strArray[i] == ' ') {
+        temp = 
+        numberArray[i] = Integer.parseInt(strArray[i - 1]);
+        System.out.println(numberArray[i] + "!!!!!!!");
+      }
+    }
   }
-}
+} 
