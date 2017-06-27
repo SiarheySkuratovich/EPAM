23f882fa352d22f835397681bddb090a6c4cab88
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-06-27 23:07:02 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -7,9 +7,8 @@ public class Order {
     String order = in.nextLine();
     System.out.println("you have written: " + order);
     String[] orderParts = order.split(" ");
-    int Length = orderParts.length;
     try { 
-      for (int i = 0; i < Length - 1; i++) {
+      for (int i = 0; i < orderParts.length - 1; i++) {
         if (Integer.parseInt(orderParts[i + 1]) > Integer.parseInt(orderParts[i])) {
           System.out.println("the order is NOT decreasing");
           return;
