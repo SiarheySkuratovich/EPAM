024ee49ebe8d9d6c65f3d42e36cd7a9f1f05e052
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-06-28 13:49:20 +0300

    cannot find variable orderParts

--- Order.java
+++ Order.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class Order {
   public static void main(String[] args) {
+    if (args.length == 0) {
     System.out.println("Write down any order of numbers separated by a space");
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);    
     String order = in.nextLine();
     System.out.println("you have written: " + order);
     String[] orderParts = order.split(" ");
+    } else {
+      String[] orderParts = new String[args.length];
+      for(int i = 0; i < args.length; i++) {
+        orderParts[i] = args[i];
+      }
+    }
     try { 
       for (int i = 0; i < orderParts.length - 1; i++) {
         if (Integer.parseInt(orderParts[i + 1]) > Integer.parseInt(orderParts[i])) {
