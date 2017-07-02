46957b1f300024b0c86c5aada38676f0b91ca91a
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-07-02 15:15:45 +0300

    added javadoc, separated by classes

---
+++ Analysis.java
@@ -0,0 +1,17 @@
+/**
+ *This class contains analyze() method
+ */
+public class Analysis {
+    /**
+     *The method determines whether the order is increasing.
+     *@param orderParts a string we get from (@link Read#readString())
+     */
+    public void analyze (String[] orderParts) {
+        for (int i = 0; i < orderParts.length - 1; i++) {
+            if (Integer.parseInt(orderParts[i + 1]) > Integer.parseInt(orderParts[i])) {
+                System.out.println("the order is NOT decreasing");
+                System.exit(0);
+            }
+        }
+    }
+}
--- Order.java
+++ Order.java
@@ -1,29 +1,35 @@
-import java.util.Scanner;
 import java.util.Arrays;
 
+/**
+ *This class contains start point of the program
+ *The program determines whether the order is decreasing
+ */
 public class Order {
-  public static void main(String[] args) {
-    String[] orderParts;
-    if (args.length == 0) {
-      System.out.println("Write down any order of numbers separated by a space");
-      Scanner in = new Scanner(System.in);    
-      String order = in.nextLine();
-      System.out.println("you have written: " + order);
-      orderParts = order.split(" ");
-    } else {
-        orderParts = Arrays.copyOf(args, args.length);
-    }
-    try { 
-      for (int i = 0; i < orderParts.length - 1; i++) {
-        if (Integer.parseInt(orderParts[i + 1]) > Integer.parseInt(orderParts[i])) {
-          System.out.println("the order is NOT decreasing");
-          return;
+
+    /**
+     *Start point of the program
+     *@param args comand line values
+     *@see Analysis#analyze(String[])
+     *@see Read#readString()
+     */
+    public static void main(String[] args) {
+        String[] orderParts;
+        if (args.length == 0) {
+            System.out.println("Write down any order of numbers separated by a space");
+            Read in = new Read();
+            String order = in.readString();
+            System.out.println("you have written: " + order);
+            orderParts = order.split(" ");
+        } else {
+            orderParts = Arrays.copyOf(args, args.length);
         }
-      }  
-    } catch (NumberFormatException ex) {
-      System.out.println("It is NOT an order of numbers! Try again");
-      return;
+        Analysis takeTo = new Analysis();
+        try {
+            takeTo.analyze(orderParts);
+        } catch (NumberFormatException ex) {
+            System.out.println("It is NOT an order of numbers! Try again");
+            return;
+        }
+        System.out.println("the order is decreasing");
     }
-    System.out.println("the order is decreasing");
-  }
 }
