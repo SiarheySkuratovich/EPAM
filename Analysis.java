5355c122bdede1c73de8c36e92f17dd072c65693
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-07-05 11:03:52 +0300

    corrected

--- Analysis.java
+++ Analysis.java
@@ -1,17 +1,18 @@
 /**
- *This class contains analyze() method
+ * This class makes string-analyzing objects
  */
 public class Analysis {
-    /**
-     *The method determines whether the order is increasing.
-     *@param orderParts a string we get from (@link Read#readString())
-     */
-    public void analyze (String[] orderParts) {
-        for (int i = 0; i < orderParts.length - 1; i++) {
-            if (Integer.parseInt(orderParts[i + 1]) > Integer.parseInt(orderParts[i])) {
-                System.out.println("the order is NOT decreasing");
-                System.exit(0);
-            }
-        }
+  /**
+   * The method determines whether the order is increasing.
+   * @param orderParts a string we get from (@link Read#readString())
+   */
+  public boolean analyze (String[] orderParts) {
+    for (int i = 0; i < orderParts.length - 1; i++) {
+      if (Integer.parseInt(orderParts[i + 1]) > Integer.parseInt(orderParts[i])) {
+        System.out.println("the order is NOT decreasing");
+        return true;
+      }
     }
+    return false;
+  }
 }
--- Order.java
+++ Order.java
@@ -1,35 +1,37 @@
 import java.util.Arrays;
 
 /**
- *This class contains start point of the program
- *The program determines whether the order is decreasing
+ * This class contains start point of the program
+ * The program determines whether the order is decreasing
  */
 public class Order {
 
-    /**
-     *Start point of the program
-     *@param args comand line values
-     *@see Analysis#analyze(String[])
-     *@see Read#readString()
-     */
-    public static void main(String[] args) {
-        String[] orderParts;
-        if (args.length == 0) {
-            System.out.println("Write down any order of numbers separated by a space");
-            Read in = new Read();
-            String order = in.readString();
-            System.out.println("you have written: " + order);
-            orderParts = order.split(" ");
-        } else {
-            orderParts = Arrays.copyOf(args, args.length);
-        }
-        Analysis takeTo = new Analysis();
-        try {
-            takeTo.analyze(orderParts);
-        } catch (NumberFormatException ex) {
-            System.out.println("It is NOT an order of numbers! Try again");
-            return;
-        }
-        System.out.println("the order is decreasing");
+  /**
+   * Start point of the program
+   * @param args comand line values
+   * @see Analysis#analyze(String[])
+   * @see Read#readString()
+   */
+  public static void main(String[] args) {
+    String[] orderParts;
+    if (args.length == 0) {
+      System.out.println("Write down any order of numbers separated by a space");
+      Reading in = new Reading();
+      String order = in.readString();
+      System.out.println("you have written: " + order);
+      orderParts = order.split(" ");
+    } else {
+      orderParts = Arrays.copyOf(args, args.length);
     }
+    Analysis takeTo = new Analysis();
+    try {
+      if(takeTo.analyze(orderParts)) {
+        return;
+      }
+    } catch (NumberFormatException ex) {
+      System.out.println("It is NOT an order of numbers! Try again");
+      return;
+    }
+    System.out.println("the order is decreasing");
+  }
 }
