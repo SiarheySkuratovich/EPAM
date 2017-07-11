26f4d327266ed9038dcbbe57c9422f4099fc79ce
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-07-11 23:39:07 +0300

    corrected javadoc

--- Analysis.java
+++ Analysis.java
@@ -1,12 +1,11 @@
 /**
- * Makes string-analyzing objects
+ * Aanalyzes sequences.
  */
 public class Analysis {
   /**
    * Determines whether the order is increasing.
    * @param orderParts a string we get to analyze.
-   * @return true is NOT decreasing.
-   * @return false is decreasing.
+   * @return false if the order is decreasing and true if it is not.
    */
   public boolean analyze (String[] arrayOfnumbers) {
     for (int i = 0; i < arrayOfnumbers.length - 1; i++) {
@@ -16,6 +15,12 @@ public class Analysis {
     }
     return false;
   }
+
+  /**
+   * Checks whether the order contains less than 2 symbol.
+   * @param arrayOfsymbols a string we get to check.
+   * @return true if the order consists of 1 symbol and false if it doesn't.
+   */
   public boolean check(String[] arrayOfsymbols) {
     if (arrayOfsymbols.length == 1) {
       System.out.println("There is only one member. Try again");
