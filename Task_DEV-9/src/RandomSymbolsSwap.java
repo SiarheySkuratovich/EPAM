e13f9b306edf265ecb27d6b230a729143ee16361
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-09-29 21:49:52 +0300

    logics has changed a bit

--- Task_DEV-9/src/RandomSymbolsSwap.java
+++ Task_DEV-9/src/RandomSymbolsSwap.java
@@ -3,12 +3,13 @@ import java.util.Random;
 public class RandomSymbolsSwap {
   public String swap(String firstString, String secondString) {
     Random random = new Random();
-    int quantityOfSymbols = random.nextInt(secondString.length());
-    int fromPlaceInTheSecondString = random.nextInt(secondString.length() - quantityOfSymbols);
-    String temp = secondString.substring(fromPlaceInTheSecondString, fromPlaceInTheSecondString + quantityOfSymbols);
-    int toPlaceInTheFirstString = random.nextInt(firstString.length() - quantityOfSymbols);
+    int quantityOfSymbolsInSecond = random.nextInt(secondString.length());
+    int fromPlaceInTheSecondString = random.nextInt(secondString.length() - quantityOfSymbolsInSecond);
+    String temp = secondString.substring(fromPlaceInTheSecondString, fromPlaceInTheSecondString + quantityOfSymbolsInSecond);
+    int toPlaceInTheFirstString = random.nextInt(firstString.length());
     String PartOfFirstString = firstString.substring(0, toPlaceInTheFirstString).concat(temp);
-    String finalFirstString = PartOfFirstString.concat(firstString.substring(toPlaceInTheFirstString + quantityOfSymbols));
+    int quantityOfSymbolsInFirst = random.nextInt(firstString.length() - toPlaceInTheFirstString);
+    String finalFirstString = PartOfFirstString.concat(firstString.substring(toPlaceInTheFirstString + quantityOfSymbolsInFirst));
     return finalFirstString;
   }
 }
