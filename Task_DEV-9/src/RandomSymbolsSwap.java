98f0d10ebd7e8759a6e0b51f2a6e0b9bdb6e61f4
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-09-30 21:24:37 +0300

    Revert "logics has changed a bit"
    This reverts commit e13f9b306edf265ecb27d6b230a729143ee16361.

--- Task_DEV-9/src/RandomSymbolsSwap.java
+++ Task_DEV-9/src/RandomSymbolsSwap.java
@@ -3,13 +3,12 @@ import java.util.Random;
 public class RandomSymbolsSwap {
   public String swap(String firstString, String secondString) {
     Random random = new Random();
-    int quantityOfSymbolsInSecond = random.nextInt(secondString.length());
-    int fromPlaceInTheSecondString = random.nextInt(secondString.length() - quantityOfSymbolsInSecond);
-    String temp = secondString.substring(fromPlaceInTheSecondString, fromPlaceInTheSecondString + quantityOfSymbolsInSecond);
-    int toPlaceInTheFirstString = random.nextInt(firstString.length());
+    int quantityOfSymbols = random.nextInt(secondString.length());
+    int fromPlaceInTheSecondString = random.nextInt(secondString.length() - quantityOfSymbols);
+    String temp = secondString.substring(fromPlaceInTheSecondString, fromPlaceInTheSecondString + quantityOfSymbols);
+    int toPlaceInTheFirstString = random.nextInt(firstString.length() - quantityOfSymbols);
     String PartOfFirstString = firstString.substring(0, toPlaceInTheFirstString).concat(temp);
-    int quantityOfSymbolsInFirst = random.nextInt(firstString.length() - toPlaceInTheFirstString);
-    String finalFirstString = PartOfFirstString.concat(firstString.substring(toPlaceInTheFirstString + quantityOfSymbolsInFirst));
+    String finalFirstString = PartOfFirstString.concat(firstString.substring(toPlaceInTheFirstString + quantityOfSymbols));
     return finalFirstString;
   }
 }
