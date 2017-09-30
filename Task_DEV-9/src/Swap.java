907b45877372e78279b0cb43ea94e5e6b463d61e
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-09-30 21:18:58 +0300

    corrected logics, added try-catch

--- Task_DEV-9/src/Swap.java
+++ Task_DEV-9/src/Swap.java
@@ -10,14 +10,18 @@ public class Swap {
    * @param args comand line values
    */
   public static void main(String[] args) {
-    SymbolsSwap symbolsSwap = new SymbolsSwap();
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter any string, please");
-    String firstString = in.nextLine();
-    System.out.println("Enter another one, please");
-    String secondString = in.nextLine();
-    System.out.println("First String you have written: " + firstString);
-    System.out.println("Second String you have written: " + secondString);
-    System.out.println("Swapped string is following: " + symbolsSwap.swapPiecesRandomly(firstString, secondString));
+    try {
+      SymbolsSwap symbolsSwap = new SymbolsSwap();
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter any string, please");
+      String firstString = in.nextLine();
+      System.out.println("Enter another one, please");
+      String secondString = in.nextLine();
+      System.out.println("First String you have written: " + firstString);
+      System.out.println("Second String you have written: " + secondString);
+      System.out.println("Swapped string is following: " + symbolsSwap.swapPiecesRandomly(firstString, secondString));
+    } catch (java.lang.IllegalArgumentException e) {
+      System.out.println("You have forgotten to enter a string or string is only one symbol. Please, try again.");
+    }
   }
 }
--- Task_DEV-9/src/SymbolsSwap.java
+++ Task_DEV-9/src/SymbolsSwap.java
@@ -12,14 +12,22 @@ public class SymbolsSwap {
    * @param secondString a string we gets to cut symbols from it.
    * @return first string after swapping.
    */
-  public String swapPiecesRandomly(String firstString, String secondString) {
+  public String swapPiecesRandomly(String firstString, String secondString) throws java.lang.IllegalArgumentException {
+    if (firstString.length() == 1 || secondString.length() == 1) {
+      throw new  java.lang.IllegalArgumentException("Requires a sequence of characters. Not one symbol.");
+    }
     Random random = new Random();
     int quantityOfSymbolsInSecond = random.nextInt(secondString.length() - 1) + 1;
     int fromPlaceInTheSecondString = random.nextInt(secondString.length() - quantityOfSymbolsInSecond);
     String temp = secondString.substring(fromPlaceInTheSecondString, fromPlaceInTheSecondString + quantityOfSymbolsInSecond);
     int toPlaceInTheFirstString = random.nextInt(firstString.length());
     String PartOfFirstString = firstString.substring(0, toPlaceInTheFirstString).concat(temp);
-    int quantityOfSymbolsInFirst = random.nextInt(firstString.length() - toPlaceInTheFirstString - 1) + 1;
+    int quantityOfSymbolsInFirst = 0;
+    if (firstString.length() == toPlaceInTheFirstString + 1) {
+      quantityOfSymbolsInFirst = 1;
+    } else {
+      quantityOfSymbolsInFirst = random.nextInt(firstString.length() - toPlaceInTheFirstString - 1) + 1;
+    }
     firstString = PartOfFirstString.concat(firstString.substring(toPlaceInTheFirstString + quantityOfSymbolsInFirst));
     return firstString;
   }
