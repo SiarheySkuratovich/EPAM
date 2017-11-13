b84f33ac6567d711d9873f75816414769db715a5
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-11-14 01:56:14 +0300

    logics is built

---
+++ test_01.task_02/src/main/java/CountedCollection.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * Created by siarhey on 13.11.17.
+ */
+public class CountedCollection {
+  private HashMap<Object, Integer> hashMap;
+
+  public CountedCollection(ArrayList arrayList) {
+    hashMap = new HashMap<Object, Integer>();
+    Integer am;
+    for (Object i : arrayList) {
+      am = hashMap.get(i);
+      hashMap.put(i, am == null ? 1 : am + 1);
+    }
+  }
+
+  public void removeNonRepeatable() {
+    for(Object key : hashMap.keySet().toArray()) {
+      if (hashMap.get(key) == 1) {
+        hashMap.remove(key);
+      }
+    }
+  }
+
+  public Set<Map.Entry<Object,Integer>> entrySet()   {
+    return hashMap.entrySet();
+  }
+}
---
+++ test_01.task_02/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Created by siarhey on 13.11.17.
+ */
+public class Main {
+  public static void main(String[] args) {
+
+
+    ArrayList arrayList = new ArrayList();
+    arrayList.add(1);
+    arrayList.add(2);
+    arrayList.add(1);
+    arrayList.add(2);
+    arrayList.add(1);
+    arrayList.add(3);
+    CountedCollection countedCollection = new CountedCollection(arrayList);
+    countedCollection.removeNonRepeatable();
+    System.out.println("Quantity of repeatable objects is: " + countedCollection.entrySet().size());
+  }
+}
