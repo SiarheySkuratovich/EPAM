544cd98a65e3dae57c7245d4caa6762952ec9173
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-11-14 16:58:14 +0300

    try-catch, logger

--- task_1/src/main/java/Main.java
+++ task_1/src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Validates some object path of the file system in Windows OS.
  */
 public class Main {
+  private static Logger log = Logger.getLogger(Main.class.getName());
 
   /**
    * Entry-point of the program.
@@ -11,28 +14,32 @@ public class Main {
    * @param args separated by spaces path.
    */
   public static void main(String[] args) {
-    Path path = new Path(String.join(" ", args));
-    if (path.containsForbiddenCharacters()) {
-      System.out.println("Invalid path!");
-      return;
+    try {
+      Path path = new Path(String.join(" ", args));
+      if (path.containsForbiddenCharacters()) {
+        System.out.println("Invalid path!");
+        return;
+      }
+      if (!path.checkForValidFolderEndings()) {
+        System.out.println("Invalid path!");
+        return;
+      }
+      if (path.containsRepeatingSlashes()) {
+        System.out.println("Invalid path!");
+        return;
+      }
+      if (path.сontainsReservedNames()) {
+        System.out.println("Invalid path!");
+        return;
+      }
+      if (!path.isPathWithValidNameLengths()) {
+        System.out.println("Invalid path!");
+        return;
+      }
+      System.out.println("Path is valid");
+    } catch (Exception e) {
+      log.log(Level.SEVERE, "Exception:  ", e);
     }
-    if (!path.checkForValidFolderEndings()) {
-      System.out.println("Invalid path!");
-      return;
-    }
-    if (path.containsRepeatingSlashes()) {
-      System.out.println("Invalid path!");
-      return;
-    }
-    if (path.сontainsReservedNames()) {
-      System.out.println("Invalid path!");
-      return;
-    }
-    if(!path.isPathWithValidNameLengths()) {
-      System.out.println("Invalid path!");
-      return;
-    }
-    System.out.println("Path is valid");
   }
 }
 
