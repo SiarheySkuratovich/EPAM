8bc0974366a76049e0b514188f134c08882cc8ba
SiarheySkuratovich <SiarheySkuratovich@yandex.ru>
2017-11-17 20:42:48 +0300

    javadoc

--- test_01.task_04/src/main/java/Coordinate.java
+++ test_01.task_04/src/main/java/Coordinate.java
@@ -1,29 +1,48 @@
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
- * Created by siarhey on 17.11.17.
+ * Coordinate of ship.
  */
 public class Coordinate {
   private final static HashMap<Character, Integer> CONVERTER = new HashMap<Character, Integer>();
   private int letter;
   private int number;
 
+  /**
+   * fills {@link Coordinate#CONVERTER} with character-number dictionary.
+   */
   static {
     for (int i = (int)'a'; i < (int)'a' + 10; i++) {
       CONVERTER.put((char)i, i - (int)'a');
     }
   }
 
+  /**
+   * Initializes "letter" with appropriate digit of letter from gotten string and
+   *           "number" with number from gotten string.
+   * @param coordinate gotten string-coordinate to parse.
+   */
   Coordinate(String coordinate) {
     try {
       letter = CONVERTER.get(coordinate.charAt(0));
       number = Integer.parseInt(coordinate.substring(2)) - 1;
     } catch (Exception e) {
       Scanner in = new Scanner(System.in);
-      System.out.println("illegal arguments. Try again");
+      System.out.println("Illegal arguments. Try again");
       new Coordinate(in.nextLine());
     }
+  }
+
+  /**
+   * Initializes "letter" and "number" with random number within field size.
+   * @param fieldSize gotten field size.
+   */
+  Coordinate (int fieldSize) {
+    Random random = new Random();
+    letter = random.nextInt(fieldSize);
+    number = random.nextInt(fieldSize);
   }
 
   public int getLetter() {
--- test_01.task_04/src/main/java/EnemyField.java
+++ test_01.task_04/src/main/java/EnemyField.java
@@ -10,22 +10,30 @@ public class EnemyField {
   int shipQuantity;
   final int FIELD_SIZE = 10;
 
+  /**
+   * Field fills by ships in random places.
+   * And their number is counted.
+   */
   EnemyField() {
     field = new boolean[FIELD_SIZE][FIELD_SIZE];
     shipQuantity = 0;
     fillWithFalse();
     Random random = new Random();
     for (int i = 0; i < random.nextInt(FIELD_SIZE - 1) + 1; i++) {
-      int letter = random.nextInt(FIELD_SIZE);
-      int number = random.nextInt(FIELD_SIZE);
-      if(!(checkHorizontally(letter, number) || checkVertically(letter, number) ||
-              checkFirstDiagonal(letter, number) || checkSecondDiagonal(letter, number))) {
+      Coordinate randomCoordinate = new Coordinate(FIELD_SIZE);
+      int letter = randomCoordinate.getLetter();
+      int number = randomCoordinate.getNumber();
+      if(!(checkHorizontally(randomCoordinate) || checkVertically(randomCoordinate) ||
+              checkFirstDiagonal(randomCoordinate) || checkSecondDiagonal(randomCoordinate))) {
         field[letter][number] = true;
         shipQuantity++;
       }
     }
   }
 
+  /**
+   * Returns disposition of ships as HashMap.
+   */
   public HashMap<Character, Integer> getHint() {
     HashMap<Character, Integer> hint = new HashMap<Character, Integer>();
     final int numberOfFirstLatinSymbol = 97;
@@ -43,43 +51,83 @@ public class EnemyField {
     return shipQuantity;
   }
 
+  /**
+   * Check whether a ship in the coordinate.
+   * @param coordinate gotten coordinate to check.
+   * Returns false if there is not a ship and true if vice versa.
+   */
   public boolean contains(Coordinate coordinate) {
     return field[coordinate.getLetter()][coordinate.getNumber()];
   }
 
+  /**
+   * "kills" a ship in gotten coordinate.
+   * @param coordinate gotten coordinate to kill a ship there.
+   */
   public void kill(Coordinate coordinate) {
     shipQuantity--;
     field[coordinate.getLetter()][coordinate.getNumber()] = false;
   }
 
-  private boolean checkHorizontally(int letter, int number) {
+  /**
+   * Returns true whether a ship can stands in specified coordinate and false if can't.
+   * Сhecking is carried out horizontally
+   * @param coordinate gotten coordinate to check.
+   */
+  private boolean checkHorizontally(Coordinate coordinate) {
+    int letter = coordinate.getLetter();
+    int number = coordinate.getNumber();
     if (number != 0 && number != FIELD_SIZE - 1) {
       return field[letter][number - 1] || field[letter][number + 1];
     }
     return false;
   }
 
-  private boolean checkVertically(int letter, int number) {
+  /**
+   * Returns true whether a ship can stands in specified coordinate and false if can't.
+   * Сhecking is carried out vertically
+   * @param coordinate gotten coordinate to check.
+   */
+  private boolean checkVertically(Coordinate coordinate) {
+    int letter = coordinate.getLetter();
+    int number = coordinate.getNumber();
     if (letter != 0 && letter != FIELD_SIZE - 1){
       return field[letter - 1][number] || field[letter + 1][number];
     }
     return false;
   }
 
-  private boolean checkFirstDiagonal(int letter, int number) {
+  /**
+   * Returns true whether a ship can stands in specified coordinate and false if can't.
+   * Сhecking is carried out on first diagonal
+   * @param coordinate gotten coordinate to check.
+   */
+  private boolean checkFirstDiagonal(Coordinate coordinate) {
+    int letter = coordinate.getLetter();
+    int number = coordinate.getNumber();
     if (letter != 0 && number != 0 && letter != FIELD_SIZE - 1 && number != FIELD_SIZE - 1) {
       return field[letter - 1][number - 1] || field[letter + 1][number + 1];
     }
     return false;
   }
 
-  private boolean checkSecondDiagonal(int letter, int number) {
+  /**
+   * Returns true whether a ship can stands in specified coordinate and false if can't.
+   * Сhecking is carried out on second diagonal
+   * @param coordinate gotten coordinate to check.
+   */
+  private boolean checkSecondDiagonal(Coordinate coordinate) {
+    int letter = coordinate.getLetter();
+    int number = coordinate.getNumber();
     if (letter != 0 && number != 0 && letter != FIELD_SIZE - 1 && number != FIELD_SIZE - 1) {
       return field[letter + 1][number - 1] || field[letter - 1][number + 1];
     }
     return false;
   }
 
+  /**
+   * Fills field with default false value.
+   */
   private void fillWithFalse() {
     for (int i = 0; i < FIELD_SIZE; i++) {
       Arrays.fill(field[i], false);
